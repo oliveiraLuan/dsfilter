1010f7b8008af6c254ac2bd1167b1df2a1653567
Luan de Oliveira <luan012012@hotmail.com>
2023-12-16 00:01:30 -0300

    Criado atributos do filter

--- src/components/Filter/index.tsx
+++ src/components/Filter/index.tsx
@@ -1,12 +1,46 @@
 import './styles.css';
 
-export default function Filter(){
+export type Props = {
+    minValue : number,
+    maxValue : number
+}
+
+export default function Filter({minValue, maxValue} : Props){
+
+    function handleClickFilter(event : any){
+        event.preventDefault();
+        console.log(minValue);
+        console.log(maxValue);
+    }
+
+    function handleOnChangeMinValue(event : any){
+        minValue = event.target.value;
+    }
+
+    function handleOnChangeMaxValue(event : any){
+        maxValue = event.target.value;
+    }
+
     return (
     <div className="filter-container">
         <form action="submit">
-            <input type="text" name="minValue" id="minValue" placeholder="Preço mínimo" />
-            <input type="text" name="maxValue" id="maxValue" placeholder="Preço máximo" />
-            <button>Filtrar</button>
+            <input 
+            value={maxValue}
+            type="text" 
+            name="minValue" 
+            id="minValue" 
+            placeholder="Preço mínimo" 
+            onChange={handleOnChangeMinValue}
+            />
+            <input 
+            value={minValue}
+            type="text" 
+            name="maxValue" 
+            id="maxValue" 
+            placeholder="Preço máximo"
+            onChange={handleOnChangeMaxValue}
+            />
+            <button onClick={handleClickFilter}>Filtrar</button>
         </form>
     </div>
     );
